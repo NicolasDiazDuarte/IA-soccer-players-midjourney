2590dece841badb7c9a99c637ede41420702294e
Nicolas Diaz <nicolas.diaz@avvy.co>
2023-03-26 00:43:26 -0300

    some responsive and css improvements

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,35 +2,30 @@ import React from 'react'
 import '../card.scss'
 
 const Card = ({ _id, name, team, photo, version, nftID, rating, age, position }) => (
-  <div className="group relative shadow-card cards--three cards">
+  <div class="group relative shadow-card cards--three cards rounded-lg">
     <span class="cards--three__rect-2">
       <span class="shadow-2"></span>
     </span>
-    <ul className="cards--three__list">
-      <li className="p-1 text-[#77f0ba] font-extrabold">{rating}</li>
+    <ul class="cards--three__list">
+      <li class="p-1 text-[#77f0ba] font-extrabold">{rating}</li>
     </ul>
-    <img className=" h-auto  border-4 border-black object-cover rounded-t-xl" src={photo} />
+    <img class="h-auto border-4 border-black object-cover rounded-t-xl" src={photo} />
     <span class="cards--three__circle"></span>
-    <div className="  border-2 border-black flex-col object-cover max-h-[94.5%] bottom-0 left-0 right-0  bg-gradient-to-r from-[#e1d5e6] to-neutral-100 p-2 rounded-b-md">
-      <div className="flex justify-between items-center gap-2">
-        <p className="text-[#666e75] text-sm overflow-y-auto prompt">#{nftID}</p>
-        {/* <button
-          type="button"
-          onClick={() => downloadImage(_id, photo)}
-          className="outline-none bg-transparent border-none"
-        >
-          <img src={download} alt="download" className="w-6 h-6 object-contain" />
-        </button> */}
+    <div class="border-2 border-black max-h-[94.5%] bg-blue-100 p-2 rounded-b-md shadow-md flex flex-col justify-between">
+      <div class="flex justify-between items-center mb-2">
+        <p class="text-[#666e75] text-sm font-bold uppercase tracking-widest">#{nftID}</p>
+        <div class="text-white bg-black rounded-full text-xl font-extrabold px-2 py-1">{position}</div>
       </div>
-      <div className="flex items-center">
-        <p className="text-black text-xl font-extrabold truncate">{name}</p>
-        <div className="text-white border-2 bg-black rounded-full ml-auto text-xl font-extrabold right-0 top-0">
-          <p className="p-1">{position}</p>
+      <div class="flex flex-col justify-center items-center">
+        <div class="flex flex-col items-center">
+          <p class="text-black text-xl font-extrabold truncate text-center w-full whitespace-nowrap group-hover:text-2xl">
+            {name}
+          </p>
+          <p class="text-[#666e75] text-md font-semibold uppercase tracking-wide overflow-y-auto prompt">{team}</p>
+          <p class="text-[#666e75] text-md font-semibold uppercase tracking-wide overflow-y-auto prompt">Age: {age}</p>
         </div>
       </div>
-      <p className="text-[#666e75] text-md  overflow-y-auto prompt">Team: {team}</p>
-      <p className="text-[#666e75] text-md  overflow-y-auto prompt">Age: {age}</p>
-      <p className="flex justify-center mt-1 text-white text-xl rounded-full border-1 bg-black font-extrabold m-auto prompt">
+      <p class="flex justify-center mt-2 text-black text-2xl rounded-full border-2 bg-gradient-to-t from-[#a8dadc] to-[#d5f5f6] font-extrabold px-4 py-1 prompt group-hover:from-[#bbeeff] group-hover:to-[#ffb7c5]">
         {version}
       </p>
     </div>
